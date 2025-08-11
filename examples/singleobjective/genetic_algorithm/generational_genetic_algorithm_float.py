1787f3ac866d01a4eaf7bb794ea1879534985ab7
wlpinjlte <wlpinjlte@gmail.com>
2025-08-11 10:42:05 +0200

    add 100 attempts

--- examples/singleobjective/genetic_algorithm/generational_genetic_algorithm_float.py
+++ examples/singleobjective/genetic_algorithm/generational_genetic_algorithm_float.py
@@ -5,22 +5,26 @@ from jmetal.problem.singleobjective.unconstrained import Rastrigin
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
 if __name__ == "__main__":
-    problem = Rastrigin(10)
+    problem = Rastrigin(1)
 
-    algorithm = GeneticAlgorithm(
-        problem=problem,
-        population_size=100,
-        offspring_population_size=1,
-        mutation=PolynomialMutation(1.0 / problem.number_of_variables(), 20.0),
-        crossover=SBXCrossover(0.9, 5.0),
-        termination_criterion=StoppingByEvaluations(max_evaluations=150000),
-    )
+    avg_fintness = 0
+    for _ in range(100):
+        algorithm = GeneticAlgorithm(
+            problem=problem,
+            population_size=100,
+            offspring_population_size=1,
+            mutation=PolynomialMutation(1.0 / problem.number_of_variables(), 20.0),
+            crossover=SBXCrossover(0.9, 5.0),
+            termination_criterion=StoppingByEvaluations(max_evaluations=150000),
+        )
 
-    algorithm.run()
-    result = algorithm.result()
+        algorithm.run()
+        result = algorithm.result()
 
-    print("Algorithm: {}".format(algorithm.get_name()))
-    print("Problem: {}".format(problem.name()))
-    print("Solution: {}".format(result.variables))
-    print("Fitness: {}".format(result.objectives[0]))
-    print("Computing time: {}".format(algorithm.total_computing_time))
+        print("Algorithm: {}".format(algorithm.get_name()))
+        print("Problem: {}".format(problem.name()))
+        print("Solution: {}".format(result.variables))
+        print("Fitness: {}".format(result.objectives[0]))
+        print("Computing time: {}".format(algorithm.total_computing_time))
+        avg_fintness += result.objectives[0]
+    print(f"Avg fitness:{avg_fintness/100}")
